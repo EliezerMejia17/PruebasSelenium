1cc50b80d9fb3c9b39bd449102c3d818facb53e7
EliezerMejia17 <151650538+EliezerMejia17@users.noreply.github.com>
2024-04-10 20:31:28 -0400

    Add files via upload

---
+++ Tools/HerramientaReportes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AventStack.ExtentReports.Reporter;
+using AventStack.ExtentReports;
+
+namespace PruebasSelenium.Tools
+{
+    public class HerramientaReportes
+    {
+        private static ExtentReports reporte;
+        private static ExtentSparkReporter ReporteHTML;
+        public static ExtentReports ObtenerReporte()
+        {
+            if (reporte == null)
+            {
+                string Ruta = @"C:\Users\Eliezer\Documents\PruebasSelenium\Reports\Reporte.html";
+                ReporteHTML = new ExtentSparkReporter(Ruta);
+                reporte = new ExtentReports();
+                reporte.AttachReporter(ReporteHTML);
+            }
+            return reporte;
+        }
+
+    }
+}
---
+++ Tools/HerramientaWait.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebasSelenium.Tools
+{
+    public class HerramientaWait
+    {
+        public static bool ElementoEstaPresente(IWebDriver driver, By ubicacion, int segundos = 3)
+        {
+            try
+            {
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(segundos));
+                wait.Until(drv => drv.FindElement(ubicacion));
+                return true;
+            }
+            catch { }
+
+            return false;
+        }
+    }
+}
